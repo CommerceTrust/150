f11efe297a4acaca8c010b187908a65a33deda81
ben gandhi-shepard <ben@integritystl.com>
2015-07-16 12:26:35 -0500

    building project and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,49 @@
 // Paths
-var dest = "./";
-var src  = './src';
-
-
-var sources = {
-  jade: "jade/**/*.jade",
-  stylus: "stylus/**/*.stylus",
-};
-
-var destinations = {
-  html: "public/",
-  css: "public/css"
-};
+// var dest = "./";
+// var src  = './src';
+//
+//
+// var destinations = {
+//   html: "public/",
+//   css: "public/css"
+// };
 
 
 // Modules
 var gulp           = require('gulp');
+
+// Automagicly refreshes browser when you save
 var browserSync    = require('browser-sync');
 var reload         = browserSync.reload;
-var sourcemaps     = require('gulp-sourcemaps');
-var please         = require('gulp-pleeease');
+
+//PreProcessor
 var stylus         = require('gulp-stylus');
 
-//evil             = require('evil-icons');
-//var marked       = require('marked'); For :markdown filter in jade
-//var csswring     = require('gulp-stylus');
-//var html mini    = require('gulp-stylus');
+// SourceMaps for CSS and JS
+var sourcemaps     = require('gulp-sourcemaps');
 
-//var front
-//var yaml
+// PostProcessor for (auto-prefixing, minifying, and IE fallbacks)
+var please         = require('gulp-pleeease');
+
+// SVG Icon Library
+var evilIcons      = require('evil-icons');
+
+// Jade for HTML
+var jade         = require('gulp-jade');
+
+// Enable MarkDown with Jade. :markdown filter
+var marked         = require('marked');
+
+// Prevent pipe from breaking even if and error is encountered
+var plumber = require('gulp-plumber');
+
+// Used to Create a static DB
+var data           = require('gulp-data');
+var path           = require('path');
+var fs             = require('fs');
+var frontMatter    = require('gulp-front-matter');
+var rename         = require('gulp-rename');
+var yaml           = require('gulp-yaml');
 
 
 // Pleeease Post-Prosessor options
@@ -70,26 +85,52 @@ gulp.task('stylus', function () {
     .pipe(reload({ stream: true }));
 });
 
-
-gulp.task("jade", function(event) {
-  return gulp.src("jade/**/*.jade")
-    .pipe(jade({pretty: true }))
-    .pipe(evilIcons())
-    .pipe(gulp.dest(dest));
+gulp.task('yaml', function () {
+  return gulp.src(config.src)
+    .pipe(plumber())
+    .pipe(yaml({ space: 2 }))
+    .pipe(rename("index.jade.json"))
+    .pipe(gulp.dest(dest))
+    .pipe(browserSync.reload({stream:true}));
 });
 
+
+gulp.task('jade', function() {
+  return gulp.src('/src/jade/**/*.jade')
+    .pipe(plumber())
+    //.pipe(frontMatter({ property: 'data' }))
+    //.pipe(data(function(file) {
+      // Use this one when not watching and related to current page
+      //return require('./src/data/' + path.basename(file.path) + '.json');
+
+      //Use this one when watchgin JSON files related to the current page
+      //return JSON.parse(fs.readFileSync('./src/data/' + path.basename(file.path) + '.json'));
+
+      //Use this when watching a global JSON file for all pages
+    //  return JSON.parse(fs.readFileSync('./data/index.jade.json'));
+    //}))
+    .pipe(jade({ pretty: true }))
+    .pipe(evilIcons())
+    //.pipe(rename('index.html'))
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.reload({stream:true}));
+});
 
 gulp.task('browser-sync', function() {
-  browserSync({
-    files: [
-      themePath + '/**/*.php',
-      themePath + '/js/**/*.js'
-      ],
-    proxy: devUrl
-  });
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+        // ,
+        // files: [
+        //   themePath + '/**/*.php',
+        //   themePath + '/js/**/*.js'
+        //   ]
+    });
 });
 
 
-gulp.task('default', ['stylus', 'browser-sync'], function () {
+
+gulp.task('default', ['stylus', 'yaml', 'jade' ,'browser-sync'], function () {
   gulp.watch(sassWatch, ['stylus']);
 });
