71880a90b323d64bdc972dc5238d8215a3a4af8b
Kylia Miskell <kylia.miskell@integritystl.com>
2015-08-24 15:29:39 -0500

    Update 2015-08-24T20:29:38.859Z

--- js/drawer.js
+++ js/drawer.js
@@ -5,17 +5,8 @@ jQuery(function($){
       $nav = $(".header__logo--col-4-12");
 
   $btn.on( "click", function() {
-    console.log("toggling the drawer1");
+    //console.log( 'hello' );
     $(this).toggleClass( 'move' );
     $drawer.toggleClass( 'slide' );
-  });
-
-  // Auto-close the menu if we're jumping to another slide.
-  var $drawer_links = $("#drawer a:not(:first-child):not(:last-child)")
-
-  $drawer_links.on( "click", function() {
-    console.log("toggling the drawer2");
-    $btn.toggleClass( 'move' );
-    $drawer.toggleClass( 'slide' ); 
   });
 });
