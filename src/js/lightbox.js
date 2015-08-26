01a16c8ede7431c250ed92f0031d450097afbf9f
Kylia Miskell <kylia.miskell@integritystl.com>
2015-08-26 18:52:53 -0500

    Fixed lightbox video by changing from iframe to video tag. Also fixed repetition and typo on main page.

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -1,21 +1,12 @@
 jQuery(function($){
 
-  $('.video').magnificPopup({
-    type: 'iframe',
-    
-    iframe: {
-       markup: '<div class="mfp-iframe-scaler">'+
-                  '<div class="mfp-close"></div>'+
-                  '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-                  '<div class="mfp-title">Some caption</div>'+
-                '</div>'
-    },
-    callbacks: {
-      markupParse: function(template, values, item) {
-       values.title = item.el.attr('title');
-      }
-    }
-
-  });
+$('.video-trigger').magnificPopup({
+  items: {
+      src: '<div class="video-popup"><video width="640" height="480" controls=""><source src="https://d3n330amx7jftl.cloudfront.net/commerce150.mp4" type="video/mp4"></video></div>',
+      type: 'inline'
+  },
+  closeBtnInside: true,
+  showCloseBtn: true
+});
 
 });
