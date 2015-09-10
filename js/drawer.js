60add98cf63812e66c5f7931ba5aeaf0360d8565
Kylia Miskell <kylia.miskell@integritystl.com>
2015-09-10 17:44:35 -0500

    Update 2015-09-10T22:44:32.626Z

--- js/drawer.js
+++ js/drawer.js
@@ -3,13 +3,20 @@ jQuery(function($){
       $drawer = $("#drawer"),
       $nav = $(".header__logo--col-4-12");
 
-  $btn.on( "click", function() {
-    $(this).toggleClass( 'move' );
+  $btn.on( "click", toggle_menu);
+
+  // Make sure to close menu when jumping to another part of the timeline
+  var $drawer_links = $("#drawer a:not(:first-child):not(:last-child)")
+
+  $drawer_links.on( "click", toggle_menu);
+
+  function toggle_menu() {
+    $btn.toggleClass( 'move' );
     $drawer.toggleClass( 'slide' );
 
     $icon = $("#toggle i");
 
-    if( $(this).hasClass('move') ) {
+    if( $btn.hasClass('move') ) {
       $icon
         .removeClass('fa-bars')
         .addClass('fa-close');
@@ -18,14 +25,6 @@ jQuery(function($){
         .removeClass('fa-close')
         .addClass('fa-bars');
     }
-  });
-
-  // Make sure to close menu when jumping to another part of the timeline
-  var $drawer_links = $("#drawer a:not(:first-child):not(:last-child)")
-
-  $drawer_links.on( "click", function() {
-    $btn.toggleClass( 'move' );
-    $drawer.toggleClass( 'slide' ); 
-  });
+  }
 
 });
